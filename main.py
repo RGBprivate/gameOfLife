076f28c1f00350f8dfd1291c19b793e76106808b
RGBprivate <mmm.game321@gmail.com>
2022-09-25 18:30:13 +0200

    Dealing with array out of range

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import time
 
 # SETTINGS
-speed = 1.5
+speed = 2
 default_size = [5, 5]
 big_size = [10, 10]
 
 
 class Life:
-    def __init__(self, ex: bool):
+    def __init__(self, ex: bool, x, y, deb):
         self.exists = ex
+        self.deb = deb #FOR DEBUG
+        self.x = x
+        self.y = y
 
 
 canvas = []
@@ -23,6 +26,7 @@ def cycle(times=None):
     for i in range(times):
         time.sleep(speed)
         paint()
+        modify()
 
 
 def paint():
@@ -33,22 +37,62 @@ def paint():
     print()
 
 
+def modify():
+    for i in canvas:
+        for y in i:
+            a = scan(y)
+            if a != 0:
+                print(a, y.x, y.y)
+
+
+def scan(obj):
+    a = 0
+    x = obj.x
+    y = obj.y
+
+    if canvas[x - 1][y].exists:
+        a += 1
+
+    # if canvas[x - 1][y + 1].exists:
+    #     a += 1
+    # if canvas[x + 1][y - 1].exists:
+    #     a += 1
+    # if canvas[x][y - 1].exists:
+    #     a += 1
+    # if canvas[x - 1][y - 1].exists:
+    #     a += 1
+    # if canvas[x][y + 1].exists:
+    #     a += 1
+    # if canvas[x + 1][y].exists:
+    #     a += 1
+    # if canvas[x + 1][y + 1].exists:
+    #     a += 1
+
+    return a
+
+
 def c_canvas(w, h):
     for i in range(h):
         canvas.append([])
         for y in range(w):
-            canvas[i].append(Life(False))
+            canvas[i].append(Life(False, i, y, False))
 
 
 def exists(obj):
     if obj.exists:
         return "X"
+    elif obj.deb:
+        return "C" #FOR DEBUG
     else:
         return "_"
 
 
 def exe():
     c_canvas(big_size[0], big_size[1])
+
+    canvas[4][4].exists = True
+    canvas[5][5].exists = True
+
     cycle(100)
 
 
