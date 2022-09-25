5f979401e636ff3383c65df6eb5efb32f3fda975
RGBprivate <mmm.game321@gmail.com>
2022-09-25 20:25:17 +0200

    It's a LIFE!!!

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import time
+import random
 
 # SETTINGS
-speed = 2
+speed = 0.8
+
+starting_life = 40
+
+iterations = 100
+
 default_size = [5, 5]
 big_size = [10, 10]
 
-
 class Life:
-    def __init__(self, ex: bool, x, y, deb):
+    def __init__(self, ex: bool, x, y):
         self.exists = ex
-        self.deb = deb #FOR DEBUG
         self.x = x
         self.y = y
 
@@ -22,6 +26,7 @@ def cycle(times=None):
         while True:
             time.sleep(speed)
             paint()
+            modify()
 
     for i in range(times):
         time.sleep(speed)
@@ -41,8 +46,11 @@ def modify():
     for i in canvas:
         for y in i:
             a = scan(y)
-            if a != 0:
-                print(a, y.x, y.y)
+            if not y.exists and a == 3:
+                y.exists = True
+            elif y.exists and a <= 1 or a >= 4:
+                y.exists = False
+
 
 
 def scan(obj):
@@ -50,23 +58,29 @@ def scan(obj):
     x = obj.x
     y = obj.y
 
-    if canvas[x - 1][y].exists:
+    if x != 0 and canvas[x - 1][y].exists:
         a += 1
 
-    # if canvas[x - 1][y + 1].exists:
-    #     a += 1
-    # if canvas[x + 1][y - 1].exists:
-    #     a += 1
-    # if canvas[x][y - 1].exists:
-    #     a += 1
-    # if canvas[x - 1][y - 1].exists:
-    #     a += 1
-    # if canvas[x][y + 1].exists:
-    #     a += 1
-    # if canvas[x + 1][y].exists:
-    #     a += 1
-    # if canvas[x + 1][y + 1].exists:
-    #     a += 1
+    if x != 0 and y + 1 < len(canvas[0]) and canvas[x - 1][y + 1].exists:
+        a += 1
+
+    if x + 1 < len(canvas) and y != 0 and canvas[x + 1][y - 1].exists:
+        a += 1
+
+    if y != 0 and canvas[x][y - 1].exists:
+        a += 1
+
+    if x != 0 and y != 0 and canvas[x - 1][y - 1].exists:
+        a += 1
+
+    if y + 1 < len(canvas[0]) and canvas[x][y + 1].exists:
+        a += 1
+
+    if x + 1 < len(canvas) and canvas[x + 1][y].exists:
+        a += 1
+
+    if y + 1 < len(canvas[0]) and x + 1 < len(canvas) and canvas[x + 1][y + 1].exists:
+        a += 1
 
     return a
 
@@ -75,25 +89,21 @@ def c_canvas(w, h):
     for i in range(h):
         canvas.append([])
         for y in range(w):
-            canvas[i].append(Life(False, i, y, False))
+            canvas[i].append(Life(False, i, y))
 
 
 def exists(obj):
     if obj.exists:
         return "X"
-    elif obj.deb:
-        return "C" #FOR DEBUG
     else:
         return "_"
 
 
 def exe():
     c_canvas(big_size[0], big_size[1])
-
-    canvas[4][4].exists = True
-    canvas[5][5].exists = True
-
-    cycle(100)
+    for i in range(26):
+        canvas[random.randint(0, 9)][random.randint(0, 9)].exists = True
+    cycle(iterations)
 
 
 exe()
